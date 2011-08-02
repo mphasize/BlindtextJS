b6605b70d0a54197775d28c6b537fe8abe744928
Marcus Paeschke <marcus@mphasize.de>
2011-08-02 16:53:08 +0200

    Added 'lang' support and some more dummy text samples

--- blindtext.js
+++ blindtext.js
@@ -1,20 +1,27 @@
 var Blindtext = {
 	defaults : {
-		length : 150
+		length : 150,
+		lang: 'lorem'
 	},
-	sample : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna.",
-	get : function (length) {
+	samples : {
+		lorem : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. ",
+		en : "This is my take at an English dummy text. I'm just writing it myself right now, since didn't want to accidentally include any copyrighted material. Please help by suggesting nice, free dummy text in English!",
+		de : "Ich sitze in meinem Zimmer im Hauptquartier des Lärms der ganzen Wohnung. Alle Türen höre ich schlagen, durch ihren Lärm bleiben mir nur die Schritte der zwischen ihnen Laufenden erspart, noch das Zuklappen der Herdtüre in der Küche höre ich. Der Vater durchbricht die Türen meines Zimmers und zieht im nachschleppenden Schlafrock durch, aus dem Ofen im Nebenzimmer wird die Asche gekratzt, Valli fragt, durch das Vorzimmer Wort für Wort rufend, ob des Vaters Hut schon geputzt ist, ein Zischen, das mir befreundet sein will, erhebt noch das Geschrei einer antwortenden Stimme. Die Wohnungstüre wird aufgeklinkt und lärmt, wie aus katarrhalischem Hals, öffnet sich dann weiterhin mit dem Singen einer Frauenstimme und schliesst sich endlich mit einem dumpfen, männlichen Ruck, der sich am rücksichtslosesten anhört. Der Vater ist weg, jetzt beginnt der zartere, zerstreutere, hoffnungslosere Lärm, von den Stimmen der zwei Kanarienvögel angeführt. Schon früher dachte ich daran, bei den Kanarienvögeln fällt es mir von neuem ein, ob ich nicht die Türe bis zu einer kleinen Spalte öffnen, schlangengleich ins Nebenzimmer kriechen und so auf dem Boden meine Schwestern und ihr Fräulein um Ruhe bitten sollte. Franz Kafka."
+	},
+	get : function (length, lang) {
 		if(typeof length == 'string') length = parseInt(length);	
-		var text = Blindtext.sample;
+		var sample = Blindtext.samples[lang] || Blindtext.samples.lorem;
+		var text = sample;
 		while(text.length < length) {
-			text += " " + Blindtext.sample;
+			text += " " + sample;
 		}
-		return Blindtext.sample.substr(0,length);
+		return text.substr(0,length);
 	},
 	replace : function () {
 		$("blindtext").each(function (i, text) {
-			var length = $(text).attr('length') || Blindtext.defaults.length;
-			$(text).replaceWith(Blindtext.get(length));	
+			var length = $(text).attr('length') || Blindtext.defaults.length;			
+			var lang = $(text).attr('lang') || Blindtext.defaults.lang;						
+			$(text).replaceWith(Blindtext.get(length, lang));	
 		});
 	}
 }
